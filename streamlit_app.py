c0a8c0fa3f2227b7096221f2f99ee55aacd5a0f1
yash-snow1 <109511902+yash-snow1@users.noreply.github.com>
2022-07-18 14:40:27 +0530

    Update streamlit_app.py
    Added avocado toast in the menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,8 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 and Blueberry oatmeal')
-streamlit.text('Kale, Spinach and Rocket Smoothie ')
-streamlit.text('Hard boiled free range egg')
+streamlit.text('🥣 Omega 3 and Blueberry oatmeal')
+streamlit.text('🥗 Kale, Spinach and Rocket Smoothie ')
+streamlit.text('🐔 Hard boiled free range egg')
+streamlit.text('🥑🍞 Avocado Toast')
 
