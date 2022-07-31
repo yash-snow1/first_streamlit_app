1927c985b26b565ebc7dfd5e4afbea7f09105e4b
yash-snow1 <109511902+yash-snow1@users.noreply.github.com>
2022-07-31 20:35:50 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -95,5 +95,5 @@ add_my_fruit = "jackfruit"
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
 streamlit.write('Thanks for adding', add_my_fruit)
 
-my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+# my_cur.execute("insert into fruit_load_list values ('from streamlit')")
 
